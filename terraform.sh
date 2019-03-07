1c02ed6781378b36adbbd6809a286e5a005ec45c
mivanovvs <mivanov@edgegravity.ericsson.com>
2019-03-07 17:51:42 +0200

    changed docker image

--- terraform.sh
+++ terraform.sh
@@ -3,4 +3,4 @@
 MOUNTS="-v $PWD/temp:/tmp/temp -v $PWD/egp-envs/:/egp-envs -v $PWD/egp-devops/:/egp-devops"
 
 
-docker run -it $MOUNTS -w /tmp/temp terrandocker:latest terraform $@
+docker run -it $MOUNTS -w /tmp/temp rm.cdx-dev.unifieddeliverynetwork.net/terr-ansible-test/terransible terraform $@
