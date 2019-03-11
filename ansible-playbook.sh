26bc5372a3bc37d3c7fb28bb5bfaefb4f1aef3d5
mivanovvs <mivanov@edgegravity.ericsson.com>
2019-03-11 17:20:46 +0200

    added mounts

--- ansible-playbook.sh
+++ ansible-playbook.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-MOUNTS="-v $PWD/temp:/tmp/temp -v $PWD/egp-envs/:/egp-envs -v $PWD/egp-devops/:/egp-devops -v $PWD/ssh_keys:/ssh"
+MOUNTS="-v $PWD/temp:/tmp/temp -v $PWD/egp-envs/:/egp-envs -v $PWD/egp-devops/:/egp-devops -v $PWD/ssh_keys:/ssh -v $PWD/egp-devops/platform/system/ansible/ansible.cfg:/etc/ansible/ansible.cfg"
 
+#cat /etc/ansible/ansible.cfg
 
 docker run -it $MOUNTS -w /tmp/temp rm.cdx-dev.unifieddeliverynetwork.net/terr-ansible-test/terransible ansible-playbook $@
